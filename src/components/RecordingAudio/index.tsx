64005a5e1d3dcc1c87533518bac24bd0ee08c789
Bill <dinhthien2804@gmail.com>
2023-10-12 13:20:06 +0700

    Add api upload audio to storage

--- src/components/RecordingAudio/index.tsx
+++ src/components/RecordingAudio/index.tsx
@@ -10,6 +10,7 @@ import { nextVocabulary } from "@/store/ExerciseStore";
 import { useAppDispatch } from "@/store/hook";
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { useSaveRecordMutation } from "@/core/services";
+import UploadFileController from "@/core/controllers/uploadFile.controller";
 
 export interface RecordingAudioProp {
   topicId: string;
@@ -57,49 +58,13 @@ export default function RecordingAudio({
   };
 
   const onHandleNext = async () => {
-    // const audioBlob = await fetch(mediaBlobUrl as any).then((r) => r.blob());
-    // const audiofile = new File([audioBlob], "audiofile.mp3", {
-    //   type: "audio/mp3",
-    // });
-
-    // const storageRef = ref(
-    //   firebaseStorage,
-    //   `files/${topicId}/${vocabularyId}/voice_${nanoid()}`
-    // );
-    // const uploadTask = uploadBytesResumable(storageRef, audiofile);
-
-    // uploadTask.on(
-    //   "state_changed",
-    //   (snapshot) => {
-    //     const progress = Math.round(
-    //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-    //     );
-    //     // setProgresspercent(progress);
-    //     console.log(progress);
-    //   },
-    //   (error) => {
-    //     alert(error);
-    //   },
-    //   () => {
-    //     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-    //       saveRecord({
-    //         clubStudyId: null,
-    //         userId: "idUser2JLpns9SQblwSgNigfTwF",
-    //         vocabularyId: vocabularyId,
-    //         voiceSrc:
-    //           "https://firebasestorage.googleapis.com/v0/b/my-english-accent-239fb.appspot.com/o/files%2FnewFile?alt=media&token=6f850b47-7f19-4ef0-8559-216dd673b86b&_gl=1*1y6kamq*_ga*MTA1MzU1OTI0Ni4xNjk2ODI1NjE1*_ga_CW55HF8NVT*MTY5NzA4MDA1OC4xMi4xLjE2OTcwODI3MjAuMi4wLjA.",
-    //       });
-    //     });
-    //   }
-    // );
-    saveRecord({
-      clubStudyId: null,
-      userId: "idUser2JLpns9SQblwSgNigfTwF",
-      vocabularyId: vocabularyId,
-      voiceSrc:
-        "https://firebasestorage.googleapis.com/v0/b/my-english-accent-239fb.appspot.com/o/files%2FnewFile?alt=media&token=6f850b47-7f19-4ef0-8559-216dd673b86b&_gl=1*1y6kamq*_ga*MTA1MzU1OTI0Ni4xNjk2ODI1NjE1*_ga_CW55HF8NVT*MTY5NzA4MDA1OC4xMi4xLjE2OTcwODI3MjAuMi4wLjA.",
+    const audioBlob = await fetch(mediaBlobUrl as any).then((r) => r.blob());
+    const audiofile = new File([audioBlob], "audiofile.mp3", {
+      type: "audio/mp3",
     });
-    
+
+    await UploadFileController.uploadAudio(audiofile, topicId, vocabularyId);
+
     refetch();
     setToggleSubBtn(false);
     dispatch(nextVocabulary());
