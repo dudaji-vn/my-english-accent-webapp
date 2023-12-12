682f47004192848492464e0c237b65d84efe912d
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-12 10:01:10 +0700

    Remove log
    Add condition to upload recoding

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,19 +1,19 @@
-import { Box, Avatar, Checkbox } from "@mui/material";
-import SpeakerIcon from "@/assets/icon/volume-icon.svg";
-import SpeakerFillIcon from "@/assets/icon/volume-fill-icon.svg";
-import { useState } from "react";
+import { Box, Avatar, Checkbox } from '@mui/material';
+import SpeakerIcon from '@/assets/icon/volume-icon.svg';
+import SpeakerFillIcon from '@/assets/icon/volume-fill-icon.svg';
+import { useState } from 'react';
 interface AudioPlayerType {
   voiceSrc: string;
   classes?: string;
 }
 
-export default function AudioPlayer({ voiceSrc, classes = "w-5 h-5" }: AudioPlayerType) {
+export default function AudioPlayer({ voiceSrc, classes = 'w-5 h-5' }: AudioPlayerType) {
   const [playing, setPlaying] = useState(true);
   const audio = new Audio(voiceSrc);
 
   const onPlay = () => {
     const isPlay = playing;
-    console.log(isPlay);
+
     if (isPlay) {
       audio.play();
     } else {
@@ -23,7 +23,12 @@ export default function AudioPlayer({ voiceSrc, classes = "w-5 h-5" }: AudioPlay
   };
   return (
     <Box>
-      <Checkbox className={classes} onClick={onPlay} icon={<Avatar src={SpeakerIcon} className='w-6 h-6' />} checkedIcon={<Avatar src={SpeakerFillIcon} className='w-6 h-6' />} />
+      <Checkbox
+        className={classes}
+        onClick={onPlay}
+        icon={<Avatar src={SpeakerIcon} className="w-6 h-6" />}
+        checkedIcon={<Avatar src={SpeakerFillIcon} className="w-6 h-6" />}
+      />
     </Box>
   );
 }
