6f662481dee89a229a0c83529bd5301161195bc8
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-19 09:44:16 +0700

    Add base64 mic recorder

--- src/components/useMicRecorder/index.tsx
+++ src/components/useMicRecorder/index.tsx
@@ -1,21 +1,31 @@
 import MicRecorder from "mic-recorder-to-mp3";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 
 type StatusType = "recording" | "stopped" | "idle";
 export default function useMicRecorder() {
   const [status, setStatus] = useState<StatusType>("idle");
   const [mediaFile, setMediaFile] = useState<File | null>(null);
+  const [mediaBase64, setMediaBase64] = useState<string>("");
 
-  const recorder = useMemo(() => {
-    return new MicRecorder({
-      bitRate: 128,
+  const [recorder, setRecorder] = useState(new MicRecorder({ bitRate: 128 }));
+
+  const toBase64 = (file: File) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const base64String = reader.result?.toString().replace("data:", "").replace(/^.+,/, "");
+        //const base64String = reader.result?.toString();
+
+        return resolve(base64String);
+      };
+      reader.onerror = reject;
     });
-  }, []);
 
   const startRecording = () => {
     recorder
       .start()
-      .then(() => {
+      .then((stream: any) => {
         setStatus(() => "recording");
       })
       .catch((e: any) => {
@@ -29,12 +39,13 @@ export default function useMicRecorder() {
     recorder
       .stop()
       .getMp3()
-      .then(([buffer, blob]: [buffer: any, blob: Blob]) => {
+      .then(([buffer, blob]: [buffer: Int8Array[], blob: Blob]) => {
         const file = new File(buffer, "recording.mp3", {
           type: blob.type,
           lastModified: Date.now(),
         });
         setMediaFile(() => file);
+        toBase64(file).then((base64) => setMediaBase64(() => base64 as string));
       })
       .catch((e: any) => {
         alert("We could not retrieve your message");
@@ -49,6 +60,7 @@ export default function useMicRecorder() {
   return {
     status,
     mediaFile,
+    mediaBase64,
     startRecording,
     stopRecording,
     clearFile,
