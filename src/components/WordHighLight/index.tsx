7216bb063b3b8babfcc1b62473a961defda4a6cd
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-21 11:17:30 +0700

    Update UI for google detect voice

--- src/components/WordHighLight/index.tsx
+++ src/components/WordHighLight/index.tsx
@@ -1,8 +1,24 @@
 import { Typography } from "@mui/material";
-import { memo } from "react";
+import { memo, useMemo } from "react";
+import { EMPTY_TRANSCRIPT } from "@/core/type";
+import { removeSpecialCharacters } from "../../shared/utils/string.util";
 
 function WordHighlight({ sentence = "", transcript = "" }: { sentence: string; transcript: string }) {
-  return <Typography className={`mb-6 break-words text-large-medium`}>{sentence}</Typography>;
+  const isHighLight = useMemo(() => {
+    return removeSpecialCharacters(sentence).toLowerCase() === removeSpecialCharacters(transcript).toLowerCase();
+  }, [sentence, transcript]);
+
+  return (
+    <div>
+      {transcript === EMPTY_TRANSCRIPT ? (
+        <Typography className={`mb-6 break-words text-base text-gray-400`}>Sorry, we couldn't here you.</Typography>
+      ) : (
+        <Typography className={`mb-6 break-words text-base ${isHighLight ? "text-secondary" : "text-red-400"}`}>
+          {isHighLight ? sentence : transcript}
+        </Typography>
+      )}
+    </div>
+  );
 }
 
 export default memo(WordHighlight);
--- src/core/type/record.type.ts
+++ src/core/type/record.type.ts
@@ -1,4 +1,4 @@
-import { DocumentReference } from "firebase/firestore";
+export const EMPTY_TRANSCRIPT = "EMPTY_TRANSCRIPT";
 
 export interface RecordTypeResponse {
   recordId: string;
