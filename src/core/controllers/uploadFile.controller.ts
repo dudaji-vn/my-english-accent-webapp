16e8ef9a0635fb2ef42602da13a5a166839ebc05
linh <hoquanglinh2710@gmail.com>
2023-12-26 14:59:29 +0700

    Change config firebase to cloudinary

--- src/core/controllers/uploadFile.controller.ts
+++ src/core/controllers/uploadFile.controller.ts
@@ -1,13 +1,32 @@
-import { firebaseStorage } from "@/config/firebase";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-const audioPath = "audio";
+import axios from "axios";
+import { CLOUDINARY_CONFIG } from "../../shared/const/cloudinary";
+
+export interface ICloudinaryResponse {
+  url: string;
+  type: string;
+}
 const UploadFileController = {
+  // uploadAudio: async (mediaFile: File, vocabularyId: string, myId: string, isClub?: boolean) => {
+  //   const storageRef = ref(firebaseStorage, `${audioPath}/${isClub ? "club" : "nonclub"}/${vocabularyId}/${myId}`);
+  //   const fileRef = await uploadBytes(storageRef, mediaFile).then((r) => r.ref);
+  //   return getDownloadURL(fileRef)‚
+  //     .then((downloadURL) => downloadURL)
+  //     .catch((error) => error);
+  // },
   uploadAudio: async (mediaFile: File, vocabularyId: string, myId: string, isClub?: boolean) => {
-    const storageRef = ref(firebaseStorage, `${audioPath}/${isClub ? "club" : "nonclub"}/${vocabularyId}/${myId}`);
-    const fileRef = await uploadBytes(storageRef, mediaFile).then((r) => r.ref);
-    return getDownloadURL(fileRef)
-      .then((downloadURL) => downloadURL)
-      .catch((error) => error);
+    const { cloudName, cloudiaryUrl, uploadPreset } = CLOUDINARY_CONFIG;
+    const formData = new FormData();
+    formData.append(`file`, mediaFile);
+    formData.append("upload_preset", uploadPreset);
+    formData.append("cloud_name", cloudName);
+    try {
+      let res = await axios.post<ICloudinaryResponse>(cloudiaryUrl, formData);
+      console.log(res.data);
+      return res.data.url;
+    } catch (err) {
+      console.log(err);
+      return "";
+    }
   },
 };
 export default UploadFileController;
---
+++ src/shared/const/cloudinary.ts
@@ -0,0 +1,5 @@
+export const CLOUDINARY_CONFIG = {
+  cloudiaryUrl: process.env.REACT_APP_CLOUDINARY_URL || "",
+  uploadPreset: process.env.REACT_APP_UPLOAD_PRESET || "",
+  cloudName: process.env.REACT_APP_CLOUD_NAME || "",
+};
