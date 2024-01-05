2e53af4f8f71b00f81be2760c9c1b95e0edfc856
linh <hoquanglinh2710@gmail.com>
2024-01-05 17:04:23 +0700

    Update UI certificate progress page

--- src/pages/Certificate/CertificateProgress/index.tsx
+++ src/pages/Certificate/CertificateProgress/index.tsx
@@ -1,28 +1,30 @@
 import CloseIcon from "@/assets/icon/close-icon.svg";
-import Congratulation from "@/assets/icon/congratulation-icon.svg";
 import CertificateInfo from "@/components/CertificateInfo";
 import Loading from "@/components/Loading";
-import TranslationCard from "@/components/TranslationCard";
+import ModalCompleteCertificate from "@/components/Modal/ModalCompleteCertificate";
+import ModalLeaveExam from "@/components/Modal/ModalLeaveExam";
+import RecordCertificate from "@/components/RecordCertificate";
+import ResultCertificate from "@/components/ResultCertificate";
 import { useGetAllVocabulariesInLectureQuery } from "@/core/services";
 import { VocabularyTypeWithNativeLanguageResponse } from "@/core/type";
-import ROUTER from "@/shared/const/router.const";
-import { StageExercise } from "@/shared/type";
-import persist from "@/shared/utils/persist.util";
-import { Avatar, Box, Button, Container, IconButton, Typography } from "@mui/material";
+import { Avatar, Box, Container, IconButton, Typography } from "@mui/material";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 export default function CertificateProgressPage() {
   const navigate = useNavigate();
-  const myInfo = persist.getMyInfo().nickName;
+
   const { pathname } = useLocation();
   const certificateName = decodeURIComponent(pathname).replace("/certificate/", "");
-  let [searchParams] = useSearchParams();
-  const lectureId = searchParams.get("lectureId") ?? "";
+  const [isStart, setIsStart] = useState(false);
+  const [isOpenModalCompleteCertificate, setIsOpenModalCompleteCertificate] = useState(false);
+  const [isOpenModalLeaveExam, setIsOpenModalLeaveExam] = useState(false);
+  const [searchParams] = useSearchParams();
+  const certificateId = searchParams.get("certificateId") ?? "";
   const parentRef = useRef<HTMLDivElement>(null);
 
-  const { data, isFetching } = useGetAllVocabulariesInLectureQuery(lectureId);
-  const [isFinish, setIsFinish] = useState(data?.stage === StageExercise.Close);
+  const { data, isFetching } = useGetAllVocabulariesInLectureQuery(certificateId);
+  const [isFinish, setIsFinish] = useState(false);
   const [renderVocabulary, setRenderVocabulary] = useState<VocabularyTypeWithNativeLanguageResponse[]>([]);
   const vocabularies: VocabularyTypeWithNativeLanguageResponse[] = useMemo(() => {
     const vocab = data?.vocabularies ?? [];
@@ -49,7 +51,7 @@ export default function CertificateProgressPage() {
       };
 
       if (!isUpdate && index >= vocabularies.length - 1 && !isFinish) {
-        setIsFinish(true);
+        setIsOpenModalCompleteCertificate(true);
       }
       if (!isUpdate && index < vocabularies.length - 1) {
         newArr.push(vocabularies[index + 1]);
@@ -59,21 +61,44 @@ export default function CertificateProgressPage() {
   };
 
   const onHandleClose = () => {
-    navigate(-1);
+    if (isStart) {
+      setIsOpenModalLeaveExam(true);
+    } else {
+      navigate(-1);
+    }
+  };
+  const scrollToLastElement = () => {
+    if (parentRef && parentRef.current) {
+      const lastChildElement = parentRef.current!.lastElementChild;
+      lastChildElement?.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   useEffect(() => {
-    if (data?.stage === StageExercise.Close) {
-      setIsFinish(true);
-    } else {
-      setIsFinish(false);
-    }
-  }, [data?.stage]);
+    scrollToLastElement();
+  });
 
   if (isFetching) return <Loading />;
+  if (isFinish) {
+    return <ResultCertificate />;
+  }
 
   return (
     <Box className="flex flex-col grow bg-gray-100 min-h-screen">
+      <ModalCompleteCertificate
+        onConfirm={() => {
+          setIsFinish(true);
+        }}
+        open={isOpenModalCompleteCertificate}
+        onClose={() => setIsOpenModalCompleteCertificate(false)}
+      />
+      <ModalLeaveExam
+        onConfirm={() => navigate(-1)}
+        onClose={() => {
+          setIsOpenModalLeaveExam(false);
+        }}
+        open={isOpenModalLeaveExam}
+      />
       <Container className="py-4 divider bg-gray-100 sticky top-0 z-10">
         <Box className="flex items-center gap-2">
           <IconButton onClick={onHandleClose}>
@@ -82,30 +107,25 @@ export default function CertificateProgressPage() {
           <Typography className="text-large-semibold grow">{`TechTalk certificate ${certificateName}`}</Typography>
         </Box>
       </Container>
-      <CertificateInfo />
-      <Box ref={parentRef} className="text-center grow bg-gray-100">
-        {renderVocabulary.map((val: VocabularyTypeWithNativeLanguageResponse, index: number) => (
-          <TranslationCard
-            {...val}
-            key={val.vocabularyId}
-            nextVocabulary={nextVocabulary}
-            index={index}
-            totalVoca={vocabularies.length}
-          />
-        ))}
-        {isFinish && (
-          <Box className="bg-gray-100 flex flex-col items-center justify-center h-[500px] p-6 gap-2" key={myInfo}>
-            <Avatar src={Congratulation} className="w-16 h-16 mb-4"></Avatar>
-            <Typography className="text-extra-large-semibold">Nice job, {myInfo}</Typography>
-            <Typography variant={"body2"} className="mb-4">
-              You finally recorded all the lectures
-            </Typography>
-            <Button onClick={() => navigate(ROUTER.RECORD)} variant="outlined">
-              Finished
-            </Button>
-          </Box>
-        )}
-      </Box>
+      {!isStart ? (
+        <CertificateInfo
+          onConfirm={() => {
+            setIsStart(true);
+          }}
+        />
+      ) : (
+        <Box ref={parentRef} className="text-center grow bg-gray-100">
+          {renderVocabulary.slice(0, 3).map((val: VocabularyTypeWithNativeLanguageResponse, index: number) => (
+            <RecordCertificate
+              {...val}
+              key={val.vocabularyId}
+              nextVocabulary={nextVocabulary}
+              index={index}
+              totalVoca={vocabularies.length}
+            />
+          ))}
+        </Box>
+      )}
     </Box>
   );
 }
