62ca9d731c2768c5f49489e49a3da30e3b173d6a
linh <hoquanglinh2710@gmail.com>
2024-01-08 17:07:19 +0700

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
 
   important: "#root",
   theme: {
+    screens: {
+      xs: "380px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
         primary: "#7F56D9",
