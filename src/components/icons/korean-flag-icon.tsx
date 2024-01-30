7cdd048f76a28222e5cec59e9661b5b6d9f01657
linh <hoquanglinh2710@gmail.com>
2024-01-30 15:30:08 +0700

    Fix warning icons

--- src/components/icons/korean-flag-icon.tsx
+++ src/components/icons/korean-flag-icon.tsx
@@ -1,7 +1,7 @@
 const KoreanFlagIcon = () => {
   return (
     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_2252_41115)">
+      <g clipPath="url(#clip0_2252_41115)">
         <path
           d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z"
           fill="#F0F0F0"
--- src/components/icons/share-icon.tsx
+++ src/components/icons/share-icon.tsx
@@ -4,23 +4,23 @@ const ShareIcon = () => {
       <path
         d="M11.3333 9.92611L18.1666 3.09277"
         stroke="#7F56D9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M18.8333 6.42676V2.42676H14.8333"
         stroke="#7F56D9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.66675 2.42676H8.00008C3.83341 2.42676 2.16675 4.09342 2.16675 8.26009V13.2601C2.16675 17.4268 3.83341 19.0934 8.00008 19.0934H13.0001C17.1667 19.0934 18.8334 17.4268 18.8334 13.2601V11.5934"
         stroke="#7F56D9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
