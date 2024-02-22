43d7fed7d0cb0d6fd75c60510bcf75040d4253de
linh <hoquanglinh2710@gmail.com>
2024-02-22 10:17:37 +0700

    Update style for homeage page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,26 +4,30 @@ export default function HomePage() {
   return (
     <Box className="m-4 bg-white rounded-2xl shadow-md">
       <Box className="p-4">
-        <Typography>Practice speaking</Typography>
-        <Typography>
+        <Typography className="text-lg font-semibold mb-2">Practice speaking</Typography>
+        <Typography className="mb-6">
           Complete a lecture to practice your English speaking skills and compete against other TechTalkers in Leaderboards.
         </Typography>
-        <Button className="bg-[#E8DEF8] shadow-sm text-textPrimary">Start a lecture</Button>
+        <Button className="bg-[#E8DEF8] shadow-sm text-textPrimary rounded-2xl px-4">Start a lecture</Button>
       </Box>
       <Box className="p-4">
-        <Typography>Build vocabulary</Typography>
-        <Typography>Practice words and sentences where you have spoken incorrectly during the lectures</Typography>
+        <Typography className="text-lg font-semibold mb-2">Build vocabulary</Typography>
+        <Typography className="mb-6">
+          Practice words and sentences where you have spoken incorrectly during the lectures
+        </Typography>
         <Box className="flex items-center">
-          <Button className="bg-[#E8DEF8] shadow-sm text-textPrimary">Start now</Button>
+          <Button disabled variant="contained" className="shadow-sm text-textPrimary rounded-2xl px-4">
+            Start now
+          </Button>
           <Typography>0</Typography>
           <Typography>incorrect words and sentences</Typography>
         </Box>
       </Box>
       <Box className="p-4">
-        <Typography>TechTalk Certificates</Typography>
+        <Typography className="text-lg font-semibold">TechTalk Certificates</Typography>
         <Typography>Gain credibility and show off your English speaking skills through a test.</Typography>
         <Box className="flex items-center">
-          <Button className="bg-[#E8DEF8] shadow-sm text-textPrimary">Start a certificate</Button>
+          <Button className="bg-[#E8DEF8] shadow-sm text-textPrimary rounded-2xl px-4">Start a certificate</Button>
           <Typography>0/2</Typography>
           <Typography>TechTalk certificates</Typography>
         </Box>
