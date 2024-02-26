a20ca42007219493f6fa066e9ed269ebe4241bab
linh <hoquanglinh2710@gmail.com>
2024-02-26 14:13:28 +0700

    Add feature search in playlist

--- src/components/lecture-playlist/index.tsx
+++ src/components/lecture-playlist/index.tsx
@@ -1,17 +1,29 @@
 import CheckIcon from "@/assets/icon/circle-check-icon.svg";
 import UncheckIcon from "@/assets/icon/circle-uncheck-icon.svg";
+import CloseIcon from "@/assets/icon/close-icon.svg";
 import PeopleRecordIcon from "@/assets/icon/people-record-icon.svg";
 import SentencesIcon from "@/assets/icon/sentences-icon.svg";
 import WarningIcon from "@/assets/icon/warning-icon.svg";
 import { useGetLecturesAvailableQuery } from "@/core/services/listen.service";
 import { pluralize } from "@/shared/utils/pluralize.util";
-import { Avatar, Box, Button, Checkbox, Typography } from "@mui/material";
-import { useEffect } from "react";
+import { Avatar, Box, Button, Checkbox, IconButton, InputAdornment, TextField, Typography } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
 import BoxCard from "../box-card";
+import SearchIcon from "../icons/search-icon";
 import Loading from "../loading";
 
 export default function LecturePlaylist({ lectureList, setLectureList }: { lectureList: string[]; setLectureList: Function }) {
   const { data, isFetching } = useGetLecturesAvailableQuery();
+  const [textSearch, setTextSearch] = useState<string>("");
+  const filterLecturesAvailable = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+    if (!textSearch) {
+      return data;
+    }
+    return data.filter((item) => item.lectureName.toLocaleLowerCase().includes(textSearch.toLocaleLowerCase()));
+  }, [data, textSearch]);
 
   const onHandleSelect = (val: string) => {
     const existed = lectureList.includes(val);
@@ -51,38 +63,71 @@ export default function LecturePlaylist({ lectureList, setLectureList }: { lectu
 
   return (
     <>
-      <Box className="flex justify-between items-center">
-        <Typography variant="body2" className="text-extra-small-regular">
-          {pluralize(data.length ?? 0, "lecture")}
-        </Typography>
-        <Button className="text-base-semibold" onClick={onHandleSelectAll} disabled={!data.length}>
-          {data.length === lectureList.length ? "Unselect all" : "Select all"}
-        </Button>
-      </Box>
-      {data.map((val) => (
-        <BoxCard classes="flex justify-between items-start p-4" key={val.lectureId}>
-          <Box className="flex flex-col gap-4">
-            <Typography className="text-base-semibold">{val.lectureName}</Typography>
-            <Box>
-              <Typography variant="body2" className="text-extra-small-regular flex gap-2">
-                <Avatar src={SentencesIcon} component={"span"} className="w-4 h-4" />{" "}
-                {pluralize(val.totalVocabularies, "sentence")}
-              </Typography>
-              <Typography variant="body2" className="text-extra-small-regular flex gap-2">
-                <Avatar src={PeopleRecordIcon} component={"span"} className="w-4 h-4" /> {val.totalPeople} people recorded
-                {!val.totalPeople && <Avatar src={WarningIcon} component={"span"} className="w-4 h-4" />}
-              </Typography>
-            </Box>
-          </Box>
-          <Checkbox
-            onClick={() => onHandleSelect(val.lectureId)}
-            checked={lectureList.includes(val.lectureId)}
-            value={val.lectureId}
-            icon={<Avatar src={UncheckIcon} alt="uncheck-icon" className="w-5 h-5" />}
-            checkedIcon={<Avatar src={CheckIcon} alt="check-icon" className="w-5 h-5" />}
+      <Box className="flex flex-col items-center mb-4">
+        <Box className="w-full md:w-[500px] mb-6">
+          <TextField
+            value={textSearch}
+            onChange={(e) => {
+              setTextSearch(e.target.value);
+            }}
+            className="bg-[#fff]"
+            placeholder="Search"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+              endAdornment: textSearch && (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => {
+                      setTextSearch("");
+                    }}
+                  >
+                    <Avatar src={CloseIcon} className="w-6 h-6" />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
-        </BoxCard>
-      ))}
+        </Box>
+        <Box className="flex justify-between items-center md:w-[500px]">
+          <Typography variant="body2" className="text-extra-small-regular">
+            {pluralize(data.length ?? 0, "lecture")}
+          </Typography>
+          <Button className="text-base-semibold" onClick={onHandleSelectAll} disabled={!data.length}>
+            {data.length === lectureList.length ? "Unselect all" : "Select all"}
+          </Button>
+        </Box>
+      </Box>
+
+      <Box className="flex flex-col gap-4 items-center">
+        {filterLecturesAvailable.map((val) => (
+          <BoxCard classes="flex justify-between items-start p-4 w-full md:w-[500px]" key={val.lectureId}>
+            <Box className="flex flex-col gap-4">
+              <Typography className="text-base-semibold">{val.lectureName}</Typography>
+              <Box>
+                <Typography variant="body2" className="text-extra-small-regular flex gap-2">
+                  <Avatar src={SentencesIcon} component={"span"} className="w-4 h-4" />{" "}
+                  {pluralize(val.totalVocabularies, "sentence")}
+                </Typography>
+                <Typography variant="body2" className="text-extra-small-regular flex gap-2">
+                  <Avatar src={PeopleRecordIcon} component={"span"} className="w-4 h-4" /> {val.totalPeople} people recorded
+                  {!val.totalPeople && <Avatar src={WarningIcon} component={"span"} className="w-4 h-4" />}
+                </Typography>
+              </Box>
+            </Box>
+            <Checkbox
+              onClick={() => onHandleSelect(val.lectureId)}
+              checked={lectureList.includes(val.lectureId)}
+              value={val.lectureId}
+              icon={<Avatar src={UncheckIcon} alt="uncheck-icon" className="w-5 h-5" />}
+              checkedIcon={<Avatar src={CheckIcon} alt="check-icon" className="w-5 h-5" />}
+            />
+          </BoxCard>
+        ))}
+      </Box>
     </>
   );
 }
--- src/components/user-playlist/index.tsx
+++ src/components/user-playlist/index.tsx
@@ -1,19 +1,30 @@
 import BookIcon from "@/assets/icon/book-icon.svg";
 import CheckIcon from "@/assets/icon/circle-check-icon.svg";
 import UncheckIcon from "@/assets/icon/circle-uncheck-icon.svg";
+import CloseIcon from "@/assets/icon/close-icon.svg";
 import WarningIcon from "@/assets/icon/warning-icon.svg";
-import { Avatar, Box, Button, Checkbox, Typography } from "@mui/material";
-import BoxCard from "../box-card";
-import UserListen from "../user-listen";
 import { useGetUsersAvailableQuery } from "@/core/services/listen.service";
 import { PeopleistenTypeResponse } from "@/core/type/listen.type";
-
+import { Avatar, Box, Button, Checkbox, IconButton, InputAdornment, TextField, Typography } from "@mui/material";
+import BoxCard from "../box-card";
+import UserListen from "../user-listen";
 import { pluralize } from "@/shared/utils/pluralize.util";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+import SearchIcon from "../icons/search-icon";
 import Loading from "../loading";
 
 export default function UserPlaylist({ peopleList, setPeopleList }: { peopleList: string[]; setPeopleList: Function }) {
   const { data, isFetching } = useGetUsersAvailableQuery();
+  const [textSearch, setTextSearch] = useState<string>("");
+  const filterUsersAvailable = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+    if (!textSearch) {
+      return data;
+    }
+    return data.filter((item) => item.nickName.toLocaleLowerCase().includes(textSearch.toLocaleLowerCase()));
+  }, [data, textSearch]);
 
   const onHandleSelect = (val: string) => {
     const existed = peopleList.includes(val);
@@ -53,36 +64,68 @@ export default function UserPlaylist({ peopleList, setPeopleList }: { peopleList
 
   return (
     <>
-      <Box className="flex justify-between items-center">
-        <Typography variant="body2" className="text-extra-small-regular">
-          {pluralize(data.length ?? 0, "people", "")}
-        </Typography>
-        <Button className="text-base-semibold" onClick={onHandleSelectAll}>
-          {data.length === peopleList.length ? "Unselect all" : "Select all"}
-        </Button>
-      </Box>
-      {data.map((user: PeopleistenTypeResponse) => (
-        <BoxCard classes="flex justify-between items-start p-4" key={user.userId}>
-          <Box className="flex flex-col gap-2">
-            <UserListen {...user} />
-            <Typography variant="body2" className="text-extra-small-regular flex gap-2">
-              <Avatar variant="square" src={BookIcon} component={"span"} className="w-4 h-4" />
-              {`${pluralize(user.numberCompletedLectures, "lecture")}  recorded`}
-              {user.numberCompletedLectures ? null : <Avatar src={WarningIcon} component={"span"} className="w-4 h-4" />}
-              {user.numberCompletedLectures && user.numberSelectedLectures
-                ? ` / ${pluralize(user.numberSelectedLectures, "lecture")} selected`
-                : null}
-            </Typography>
-          </Box>
-          <Checkbox
-            onClick={() => onHandleSelect(user.userId)}
-            checked={peopleList.includes(user.userId)}
-            value={user.userId}
-            icon={<Avatar src={UncheckIcon} alt="uncheck-icon" className="w-5 h-5" />}
-            checkedIcon={<Avatar src={CheckIcon} alt="check-icon" className="w-5 h-5" />}
+      <Box className="flex flex-col items-center">
+        <Box className="w-full md:w-[500px] mb-6">
+          <TextField
+            value={textSearch}
+            onChange={(e) => {
+              setTextSearch(e.target.value);
+            }}
+            className="bg-[#fff]"
+            placeholder="Search"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+              endAdornment: textSearch && (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => {
+                      setTextSearch("");
+                    }}
+                  >
+                    <Avatar src={CloseIcon} className="w-6 h-6" />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
-        </BoxCard>
-      ))}
+        </Box>
+        <Box className="flex justify-between items-center md:w-[500px]">
+          <Typography variant="body2" className="text-extra-small-regular">
+            {pluralize(data.length ?? 0, "people", "")}
+          </Typography>
+          <Button className="text-base-semibold" onClick={onHandleSelectAll}>
+            {data.length === peopleList.length ? "Unselect all" : "Select all"}
+          </Button>
+        </Box>
+      </Box>
+      <Box className="flex flex-col gap-4 items-center">
+        {filterUsersAvailable.map((user: PeopleistenTypeResponse) => (
+          <BoxCard classes="flex justify-between items-start p-4 w-full md:w-[500px] " key={user.userId}>
+            <Box className="flex flex-col gap-2">
+              <UserListen {...user} />
+              <Typography variant="body2" className="text-extra-small-regular flex gap-2">
+                <Avatar variant="square" src={BookIcon} component={"span"} className="w-4 h-4" />
+                {`${pluralize(user.numberCompletedLectures, "lecture")}  recorded`}
+                {user.numberCompletedLectures ? null : <Avatar src={WarningIcon} component={"span"} className="w-4 h-4" />}
+                {user.numberCompletedLectures && user.numberSelectedLectures
+                  ? ` / ${pluralize(user.numberSelectedLectures, "lecture")} selected`
+                  : null}
+              </Typography>
+            </Box>
+            <Checkbox
+              onClick={() => onHandleSelect(user.userId)}
+              checked={peopleList.includes(user.userId)}
+              value={user.userId}
+              icon={<Avatar src={UncheckIcon} alt="uncheck-icon" className="w-5 h-5" />}
+              checkedIcon={<Avatar src={CheckIcon} alt="check-icon" className="w-5 h-5" />}
+            />
+          </BoxCard>
+        ))}
+      </Box>
     </>
   );
 }
